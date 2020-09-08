ad9a8ff5a34663c159cebc5dada1564880d11fd4
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-08 16:43:46 +0600

    delete and add contact action

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,4 @@
 export const GET_CONTACTS = 'GET_CONTACTS';
+export const DELETE_CONTACT = 'DELETE_CONTACT';
+export const ADD_CONTACT = 'ADD_CONTACT';
+
--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import TextInputGroup from '../layout/TextInputGroup';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addContact } from '../../actions/contactActions';
+import uuid from 'uuid';
 
 class AddContact extends Component {
   state = {
@@ -36,10 +40,12 @@ class AddContact extends Component {
       phone
     };
 
-    //// SUBMIT CONTACT ////
+    this.props.addContact(newContact);
+
 
     // Clear State
     this.setState({
+      id: uuid(), 
       name: '',
       email: '',
       phone: '',
@@ -96,4 +102,8 @@ class AddContact extends Component {
   }
 }
 
-export default AddContact;
+AddContact.propTypes = {
+  addContact: PropTypes.func.isRequired
+}
+
+export default connect(null, { addContact })(AddContact);
